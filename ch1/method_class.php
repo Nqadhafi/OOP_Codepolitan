385a34cbf64bdac0a6d06c82313e129f86c6aef5
Nqadhafi <qadhafi817@gmail.com>
2024-01-19 21:54:42 +0700

    Update dokumentasi di method SayHello

--- ch1/method_class.php
+++ ch1/method_class.php
@@ -9,9 +9,8 @@ class contohMethod{
     //     echo "Hallo bos " . $namaku; //Method say hello
     // }
     function SayHello(){
-        return $this->nama_depan . " " . $this->nama_belakang;
+        return $this->nama_depan . " " . $this->nama_belakang; //memanggil variable yang masih dalam satu class dengan "this"
     }
 }
 $panggil = new ContohMethod; //Pembuatan object dari class contohMethod
-echo $panggil->SayHello(); //Pemanggilan method SayHello dengan skup variable property nama
-?>
+echo $panggil->SayHello(); //Pemanggilan method SayHello
