e8b23a6745e0050b97a26e936b59fbbe0d7af978
Nqadhafi <qadhafi817@gmail.com>
2024-01-19 21:51:06 +0700

    Optimasi penulisan pemanggilan method menggunakan this->

--- ch1/method_class.php
+++ ch1/method_class.php
@@ -1,12 +1,17 @@
 <?php
 
 class contohMethod{
-    var $nama = "Nanda Qadhafi"; //Property nama
+    var $nama_depan = "Nanda Qadhafi"; //Property nama
+    var $nama_belakang = "Luhur Al-Ashr"; //Property nama
 
-    function SayHello($namaku){
-        echo "Hallo bos " . $namaku; //Method say hello
+    //revisi memakai konsep oop yang benar
+    // function SayHello($namaku){
+    //     echo "Hallo bos " . $namaku; //Method say hello
+    // }
+    function SayHello(){
+        return $this->nama_depan . " " . $this->nama_belakang;
     }
 }
 $panggil = new ContohMethod; //Pembuatan object dari class contohMethod
-echo $panggil->SayHello($panggil->nama); //Pemanggilan method SayHello dengan skup variable property nama
+echo $panggil->SayHello(); //Pemanggilan method SayHello dengan skup variable property nama
 ?>
